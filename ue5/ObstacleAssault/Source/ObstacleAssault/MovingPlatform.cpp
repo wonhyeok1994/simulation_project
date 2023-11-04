39b2d3ac68f4ca49f0e3243dc47a59b00494dfb8
wonhyuk1994 <wonhyuk1994@gmail.com>
2023-11-04 18:07:20 +0900

    2023-11-04 튜토진행

---
+++ ue5/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector LocalVector = MyVector;
+
+	LocalVector.Z += 100;
+
+	MyVector.Y = MyVector.Y + 1;
+
+	SetActorLocation(MyVector);
+
+	// Move platform forwards
+		// Get current location
+		// Add vector to taht location
+		// Set the location
+	// Send platform back if gone too far
+		// Check how far we've moved
+		// Reverse direction of motion if gone too far
+}
+
